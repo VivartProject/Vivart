f9d6345feaa9c65e2e61df0b2ec28bbce4299227
Maria-Georgiana Popescu <popescu.georgiana19200@yahoo.com>
2018-04-03 23:37:15 +0300

    Added db connection, events controller and view

---
+++ Vivart/Vivart/Context/EventContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vivart.Models;
+
+namespace Vivart.Context
+{
+    public class EventContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ Vivart/Vivart/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(Vivart.Startup))]
+namespace Vivart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
